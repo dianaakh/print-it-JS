6b71d6da31d6f960fc1044d2d67d794e5ba67f83
Diana Akhmadova <dianaakhmadova@gmail.com>
2023-12-07 14:12:12 +0100

    déplacement des bullets points + coms

--- assets/script.js
+++ assets/script.js
@@ -20,45 +20,56 @@ const slides = [
 
 
 
-// Initialisation de l'index de la diapositive actuelle avec let car la valeur de la variable va changer
+// Initialisation de l'index de la diapositive actuelle dans le carrousel avec let car la valeur de la variable va changer
 let currentSlideIndex = 0;
 
-const bannerImg = document.querySelector('.banner-img'); // Variable const car valeur ne change pas durant le code - Sélection de l'image du banner 
-const tagLine = document.querySelector('#banner p'); // Sélection du paragraphe dans le banner
-const dot = document.querySelector('.dot_selected') // Sélection du bullet point
+const bannerImg = document.querySelector('.banner-img'); // Récupère l'image du banner 
+const tagLine = document.querySelector('#banner p'); // Récupère paragraphe du banner
+const dots = document.querySelectorAll('.dot') // Récupère les bullet points
 
-// Fonction pour afficher une diapositive en fonction de son index dans le tableau
+
+
+// Fonction pour afficher une diapo en fonction de son index dans le tableau
 const showSlide = (index) => {
-    bannerImg.src = slides[index].image; // Mise à jour de l'URL de l'image du banner
-    tagLine.innerHTML = slides[index].tagLine; // Mise à jour du texte de la diapositive
-   
+    bannerImg.src = slides[index].image // MAJ de l'URL de l'image du banner avec l'URL de l'image correspondante à l'index
+    tagLine.innerHTML = slides[index].tagLine // MAJ du texte de la diapo avec le texte correspondant à l'index
+	selectedDot(index) // Mise à jour du bullet point sélectionné en fonction de l'index de la diapo actuelle
 };
 
 
-const arrowLeft = document.querySelector('.arrow_left');      // Sélection de la flèche gauche et ajout d'un event listener pour le clic
+const selectedDot = (index) => {    // Fonction pour MAJ le bullet point sélectionné en fonction de l'index actuel
+    dots.forEach((dot, dotIndex) => {
+        if (dotIndex === index) {
+            dot.classList.add('dot_selected') // Ajoute la classe dot_selected pour le point correspondant à l'index actuel
+        } else {
+            dot.classList.remove('dot_selected') // Retire la classe dot_selected des autres points de bullet
+        }
+    })
+}
+
+const arrowLeft = document.querySelector('.arrow_left');   // Sélection de la flèche gauche et ajout d'un event listener pour le clic
 if (arrowLeft) {
     arrowLeft.addEventListener('click', () => {
         // Déplacement vers la diapositive précédente tout en gérant les bords du tableau
-        currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-        showSlide(currentSlideIndex); // Afficher la diapositive correspondante
-    });
+        currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length // .length pour nb d'élements du tableau 
+        showSlide(currentSlideIndex) // Afficher la diapositive correspondante
+    })
 } 
 else {
-    console.log("Erreur");
+    alert("Erreur")
 }
 
-// Sélection de la flèche droite et ajout d'un event listener pour le clic
-const arrowRight = document.querySelector('.arrow_right');
+const arrowRight = document.querySelector('.arrow_right')
 if (arrowRight) {
     arrowRight.addEventListener('click', () => {
-        // Déplacement vers la diapositive suivante tout en gérant les bords du tableau
-        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-        showSlide(currentSlideIndex); // Afficher la diapositive correspondante
-    });
+        // Déplacement vers la diapo suivante tout en gérant les bords du tableau
+        currentSlideIndex = (currentSlideIndex + 1) % slides.length // % opérateur modulo pour revenir a la première diapo : boucle
+        showSlide(currentSlideIndex)
+    })
 } 
 else {
-    console.log("Erreur");
+    alert("Erreur")
 }
 
-// Afficher la diapositive initiale lorsque la page se charge
-showSlide(currentSlideIndex);
+// Afficher la diapo initiale lorsque la page se charge avec l'appel de la fonction(+argument)
+showSlide(currentSlideIndex)
