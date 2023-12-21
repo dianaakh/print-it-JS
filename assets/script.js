b456b044072c80e9daeedb5e8bc73fc83ea9715e
Diana Akhmadova <dianaakhmadova@gmail.com>
2023-12-21 15:46:40 +0100

    fix error

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 //évenement qui se déclenche quand le document HTML initial est complètement chargé et analysé
 document.addEventListener("DOMContentLoaded", () => {
 
-	// Tableau contenant les informations sur chaque diapositive
 	const slides = [
 		{
 			"image": "./assets/images/slideshow/slide1.jpg",
@@ -24,30 +23,29 @@ document.addEventListener("DOMContentLoaded", () => {
 	// Initialisation de l'index de la diapositive actuelle dans le carrousel
 	let index = 0;
 
-	// Appel fonction
 	eventArrows(index, slides)
 })
 
 const eventArrows = (index, slides) => {
-
-	const arrowLeft = document.querySelector('.arrow_left')   // Sélection de la flèche gauche et ajout d'un event listener pour le clic
+	const arrowLeft = document.querySelector('.arrow_left')
 	arrowLeft.addEventListener('click', () => {
 		// Déplacement vers la diapositive précédente tout en gérant les bords du tableau
-		index = (index - 1 + slides.length) % slides.length // .length pour nb d'élements du tableau 
+		index = (index - 1 + slides.length) % slides.length
 		showSlide(index, slides) // Afficher la diapositive correspondante
 	})
 
 	const arrowRight = document.querySelector('.arrow_right')
 	arrowRight.addEventListener('click', () => {
 		// Déplacement vers la diapo suivante tout en gérant les bords du tableau
-		index = (index + 1) % slides.length // % opérateur modulo pour revenir a la première diapo : boucle
+		index = (index + 1) % slides.length
 		showSlide(index, slides)
 	})
-
 }
 
-const selectedDot = (index) => {    // Fonction pour MAJ le bullet point sélectionné en fonction de l'index actuel de la diapo
-	const dots = document.querySelectorAll('.dot') // Récupère les bullet points
+// Fonction pour MAJ le bullet point sélectionné en fonction de l'index actuel de la diapo
+
+const selectedDot = (index) => {
+	const dots = document.querySelectorAll('.dot')
 	dots.forEach((dot, dotIndex) => {
 		if (dotIndex === index) {
 			dot.classList.add('dot_selected') // Ajoute la classe dot_selected pour le point correspondant à l'index actuel
@@ -58,10 +56,11 @@ const selectedDot = (index) => {    // Fonction pour MAJ le bullet point sélect
 }
 
 // Fonction pour afficher une diapo en fonction de son index dans le tableau
+
 const showSlide = (index, slides) => {
-	const bannerImg = document.querySelector('.banner-img') // Récupère l'image du banner 
+	const bannerImg = document.querySelector('.banner-img')
 	bannerImg.src = slides[index].image // MAJ de l'URL de l'image du banner avec l'URL de l'image correspondante à l'index
-	const tagLine = document.querySelector('#banner p') // Récupère paragraphe du banner
+	const tagLine = document.querySelector('#banner p')
 	tagLine.innerHTML = slides[index].tagLine // MAJ du texte de la diapo avec le texte correspondant à l'index
 	selectedDot(index) // MAJ du bullet point sélectionné en fonction de l'index de la diapo actuelle
 }
